d36f380d635752d2421f5f05db5280c5a87f98a1
Alban Linard <alban@linard.fr>
2015-07-21 15:48:42 +0200

    Remove option to deactivate cache, replace it by a function to clear it.

--- src/c3.lua
+++ src/c3.lua
@@ -88,120 +88,130 @@ Cache.__mode      = "k"
 --     > assert.is_falsy  (ok)
 --     > assert.is_truthy (err:match "linearization failed")
 
-function C3.new (options)
-  local superclass = options.superclass
+local mt = {}
+
+mt.__index = mt
+
+function mt.new (options)
+  assert (options and options.superclass)
   -- Check that superclass is callable:
   do
-    local ok, err = pcall (superclass)
+    local ok, err = pcall (options.superclass)
     if not ok and err:match "^attempt to call" then
       error "superclass is neither a function nor callable"
     end
   end
-  local unpack = table.unpack or unpack
-  -- Create cache:
-  local cache = setmetatable ({}, Cache)
-  -- Return the linearization function:
-  return function (x)
-    local seen = {}
-    local function linearize (t)
-      if options.cache then
-        local cached = cache [t]
-        if cached then
-          return cached
-        end
-      end
-      if seen [t] then
-        return {}
-      end
-      seen [t] = true
-      -- Prepare:
-      local depends = superclass (t)
-      local l, n = {}, {}
-      if depends and #depends ~= 0 then
-        depends = { unpack (depends) }
-        for i = 1, #depends do
-          depends [i] = depends [i]
-        end
-        l [#l+1] = depends
-        n [#n+1] = #depends
-        for i = 1, #depends do
-          local linearized = linearize (depends [i], seen)
-          if #linearized ~= 0 then
-            local ll = {}
-            for j = 1, #linearized do
-              local z = linearized [j]
-              if z ~= t then
-                ll [#ll+1] = z
-              end
-            end
-            l [#l+1] = ll
-            n [#n+1] = # (l [#l])
-          end
-        end
-      end
-      l [#l+1] = { t }
-      n [#n+1] = 1
-      -- Compute tails:
-      local tails = {}
-      for i = 1, #l do
-        local v = l [i]
-        for j = 1, #v do
-          local w   = v [j]
-          tails [w] = (tails [w] or 0) + 1
-        end
-      end
-      -- Compute linearization:
-      local result = {}
-      while #l ~= 0 do
-        for i = #l, 1, -1 do
-          local vl, vn   = l [i], n [i]
-          local first    = vl [vn]
-          local first_id = first
-          tails [first_id] = tails [first_id] - 1
-        end
-        local head
-        for i = #l, 1, -1 do
-          local vl, vn   = l [i], n [i]
-          local first    = vl [vn]
-          local first_id = first
-          if tails [first_id] == 0 then
-            head = first
-            break
-          end
-        end
-        if head == nil then
-          error "linearization failed"
-        end
-        result [#result + 1] = head
-        for i = 1, #l do
-          local vl, vn = l [i], n [i]
-          local first  = vl [vn]
-          if first == head then
-            n [i] = n [i] - 1
-          else
-            local first_id = first
-            tails [first_id] = tails [first_id] + 1
-          end
-        end
-        local nl, nn = {}, {}
-        for i = 1, #l do
-          if n [i] ~= 0 then
-            nl [#nl+1] = l [i]
-            nn [#nn+1] = n [i]
-          end
-        end
-        l, n = nl, nn
-      end
-      for i = 1, #result/2 do
-        result [i], result [#result-i+1] = result [#result-i+1], result [i]
-      end
-      if options.cache then
-        cache [t] = result
-      end
-      return result
-    end
-    return linearize (x)
-  end
+  return setmetatable ({
+    options = options,
+    cache   = setmetatable ({}, { __mode = "k" })
+  }, mt)
 end
 
-return C3
+function mt.clear (c3)
+  assert (getmetatable (c3) == mt)
+  c3.cache = setmetatable ({}, { __mode = "k" })
+  return c3
+end
+
+function mt.__call (c3, x)
+  assert (getmetatable (c3) == mt)
+  local unpack     = table.unpack or unpack
+  local superclass = c3.options.superclass
+  local seen       = {}
+  local function linearize (t)
+    local cached = c3.cache [t]
+    if cached then
+      return cached
+    end
+    if seen [t] then
+      return {}
+    end
+    seen [t] = true
+    -- Prepare:
+    local depends = superclass (t)
+    local l, n = {}, {}
+    if depends and #depends ~= 0 then
+      depends = { unpack (depends) }
+      for i = 1, #depends do
+        depends [i] = depends [i]
+      end
+      l [#l+1] = depends
+      n [#n+1] = #depends
+      for i = 1, #depends do
+        local linearized = linearize (depends [i], seen)
+        if #linearized ~= 0 then
+          local ll = {}
+          for j = 1, #linearized do
+            local z = linearized [j]
+            if z ~= t then
+              ll [#ll+1] = z
+            end
+          end
+          l [#l+1] = ll
+          n [#n+1] = # (l [#l])
+        end
+      end
+    end
+    l [#l+1] = { t }
+    n [#n+1] = 1
+    -- Compute tails:
+    local tails = {}
+    for i = 1, #l do
+      local v = l [i]
+      for j = 1, #v do
+        local w   = v [j]
+        tails [w] = (tails [w] or 0) + 1
+      end
+    end
+    -- Compute linearization:
+    local result = {}
+    while #l ~= 0 do
+      for i = #l, 1, -1 do
+        local vl, vn   = l [i], n [i]
+        local first    = vl [vn]
+        local first_id = first
+        tails [first_id] = tails [first_id] - 1
+      end
+      local head
+      for i = #l, 1, -1 do
+        local vl, vn   = l [i], n [i]
+        local first    = vl [vn]
+        local first_id = first
+        if tails [first_id] == 0 then
+          head = first
+          break
+        end
+      end
+      if head == nil then
+        error "linearization failed"
+      end
+      result [#result + 1] = head
+      for i = 1, #l do
+        local vl, vn = l [i], n [i]
+        local first  = vl [vn]
+        if first == head then
+          n [i] = n [i] - 1
+        else
+          local first_id = first
+          tails [first_id] = tails [first_id] + 1
+        end
+      end
+      local nl, nn = {}, {}
+      for i = 1, #l do
+        if n [i] ~= 0 then
+          nl [#nl+1] = l [i]
+          nn [#nn+1] = n [i]
+        end
+      end
+      l, n = nl, nn
+    end
+    for i = 1, #result/2 do
+      result [i], result [#result-i+1] = result [#result-i+1], result [i]
+    end
+    c3.cache [t] = result
+    return result
+  end
+  return linearize (x)
+end
+
+return mt
